4079dd66fc6955adb208a0633220f4b72faaf82d
Valera1978 <valera1978@tut.by>
2020-08-24 18:15:31 +0300

    init

--- v1awifi-vendor-blobs.mk
+++ v1awifi-vendor-blobs.mk
@@ -43,44 +43,42 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/v1awifi/proprietary/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin:$(TARGET_COPY_OUT_SYSTEM)/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin \
     vendor/samsung/v1awifi/proprietary/app/mcRegistry/ffffffffd0000000000000000000000e.tlbin:$(TARGET_COPY_OUT_SYSTEM)/app/mcRegistry/ffffffffd0000000000000000000000e.tlbin \
     vendor/samsung/v1awifi/proprietary/bin/cs:$(TARGET_COPY_OUT_SYSTEM)/bin/cs \
-    vendor/samsung/v1awifi/proprietary/bin/gpsd:$(TARGET_COPY_OUT_SYSTEM)/bin/gpsd \
-    vendor/samsung/v1awifi/proprietary/lib/egl/egl.cfg:$(TARGET_COPY_OUT_SYSTEM)/lib/egl/egl.cfg \
-    vendor/samsung/v1awifi/proprietary/lib/hw/gps.default.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/gps.default.so \
     vendor/samsung/v1awifi/proprietary/lib/hw/sensors.universal5420.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/sensors.universal5420.so \
     vendor/samsung/v1awifi/proprietary/lib/libak8963c.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libak8963c.so \
-    vendor/samsung/v1awifi/proprietary/lib/libexynoscamera.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libexynoscamera.so \
+    vendor/samsung/v1awifi/proprietary/lib/libExynosOMX_Core.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libExynosOMX_Core.so \
+    vendor/samsung/v1awifi/proprietary/lib/libExynosOMX_Resourcemanager.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libExynosOMX_Resourcemanager.so \
     vendor/samsung/v1awifi/proprietary/lib/libsecnativefeature.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsecnativefeature.so \
-    vendor/samsung/v1awifi/proprietary/lib/libvdis.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libvdis.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.AVC.Decoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.AVC.Decoder.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.AVC.Encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.AVC.Encoder.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.MPEG4.Decoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.MPEG4.Decoder.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.MPEG4.Encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.MPEG4.Encoder.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.VP8.Decoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.VP8.Decoder.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.VP8.Encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.VP8.Encoder.so \
+    vendor/samsung/v1awifi/proprietary/lib/omx/libOMX.Exynos.WMV.Decoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/omx/libOMX.Exynos.WMV.Decoder.so \
     vendor/samsung/v1awifi/proprietary/vendor/bin/sswap:$(TARGET_COPY_OUT_VENDOR)/bin/sswap \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_apsta.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_apsta.bin \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_apsta.bin_4339_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_apsta.bin_4339_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_apsta.bin_4354_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_apsta.bin_4354_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_apsta.bin_4354_a1:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_apsta.bin_4354_a1 \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_apsta.bin_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_apsta.bin_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_ibss.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_ibss.bin \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_ibss.bin_4339_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_ibss.bin_4339_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_ibss.bin_4354_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_ibss.bin_4354_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_ibss.bin_4354_a1:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_ibss.bin_4354_a1 \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_ibss.bin_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_ibss.bin_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_mfg.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_mfg.bin \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_mfg.bin_4339_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_mfg.bin_4339_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_mfg.bin_4354_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_mfg.bin_4354_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_mfg.bin_4354_a1:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_mfg.bin_4354_a1 \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_mfg.bin_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_mfg.bin_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta.bin \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta.bin_4339_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta.bin_4339_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta.bin_4354_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta.bin_4354_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta.bin_4354_a1:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta.bin_4354_a1 \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta.bin_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta.bin_a0 \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta_tput.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta_tput.bin \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/bcmdhd_sta_tput.bin_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd_sta_tput.bin_a0 \
-    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/cred.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/cred.conf \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/nvram_mfg.txt_4339_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/nvram_mfg.txt_4339_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/nvram_mfg.txt_4354_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/nvram_mfg.txt_4354_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/nvram_mfg.txt_4354_a1:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/nvram_mfg.txt_4354_a1 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/nvram_net.txt_4339_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/nvram_net.txt_4339_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/nvram_net.txt_4354_a0:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/nvram_net.txt_4354_a0 \
     vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/nvram_net.txt_4354_a1:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/nvram_net.txt_4354_a1 \
+    vendor/samsung/v1awifi/proprietary/vendor/etc/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     vendor/samsung/v1awifi/proprietary/vendor/firmware/bcm4335_V0105.0541.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4335_V0105.0541.hcd \
     vendor/samsung/v1awifi/proprietary/vendor/firmware/bcm4350_V0301.0591.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4350_V0301.0591.hcd \
     vendor/samsung/v1awifi/proprietary/vendor/firmware/fimc_is_fw2_IMX134.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fimc_is_fw2_IMX134.bin \
@@ -91,11 +89,21 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/v1awifi/proprietary/vendor/firmware/srp_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/srp_data.bin \
     vendor/samsung/v1awifi/proprietary/vendor/firmware/srp_vliw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/srp_vliw.bin \
     vendor/samsung/v1awifi/proprietary/vendor/lib/egl/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
-    vendor/samsung/v1awifi/proprietary/vendor/lib/libmalicore.bc:$(TARGET_COPY_OUT_VENDOR)/lib/libmalicore.bc \
+    vendor/samsung/v1awifi/proprietary/vendor/lib/hw/gralloc.exynos5.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.exynos5.so \
     vendor/samsung/v1awifi/proprietary/vendor/lib/libaptX_encoder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaptX_encoder.so \
     vendor/samsung/v1awifi/proprietary/vendor/lib/libaptXHD_encoder.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaptXHD_encoder.so \
-    vendor/samsung/v1awifi/proprietary/vendor/lib/liblvverx_3.20.03.so:$(TARGET_COPY_OUT_VENDOR)/lib/liblvverx_3.20.03.so \
-    vendor/samsung/v1awifi/proprietary/vendor/lib/liblvvetx_3.20.03.so:$(TARGET_COPY_OUT_VENDOR)/lib/liblvvetx_3.20.03.so \
+    vendor/samsung/v1awifi/proprietary/vendor/lib/liblvverx_3.21.13.so:$(TARGET_COPY_OUT_VENDOR)/lib/liblvverx_3.21.13.so \
+    vendor/samsung/v1awifi/proprietary/vendor/lib/liblvvetx_3.21.13.so:$(TARGET_COPY_OUT_VENDOR)/lib/liblvvetx_3.21.13.so \
+    vendor/samsung/v1awifi/proprietary/vendor/lib/libmalicore.bc:$(TARGET_COPY_OUT_VENDOR)/lib/libmalicore.bc \
     vendor/samsung/v1awifi/proprietary/vendor/lib/libprotobuf-cpp-looo.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-looo.so \
     vendor/samsung/v1awifi/proprietary/vendor/lib/libstlport.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstlport.so \
     vendor/samsung/v1awifi/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libwvdrmengine.so
+
+# gps add
+PRODUCT_COPY_FILES += \
+    vendor/samsung/v1awifi/proprietary/_gps/etc/ca.pem:$(TARGET_COPY_OUT_SYSTEM)/etc/ca.pem \
+    vendor/samsung/v1awifi/proprietary/_gps/lib/hw/gps.default.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/gps.default.so \
+    vendor/samsung/v1awifi/proprietary/_gps/vendor/bin/hw/gpsd:$(TARGET_COPY_OUT_VENDOR)/bin/hw/gpsd \
+    vendor/samsung/v1awifi/proprietary/_gps/vendor/bin/hw/android.hardware.gnss@1.0-service-samsung:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.gnss@1.0-service-samsung \
+    vendor/samsung/v1awifi/proprietary/_gps/vendor/etc/init/android.hardware.gnss@1.0-service-samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.gnss@1.0-service-samsung.rc \
+    vendor/samsung/v1awifi/proprietary/_gps/vendor/lib/hw/android.hardware.gnss@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.gnss@1.0-impl.so
